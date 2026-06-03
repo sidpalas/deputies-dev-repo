1492eb6215a22fd89de257e069bda64e76e3ae2e
DevDeputies <devdeputies@users.noreply.github.com>
2026-06-03 17:39:44 +0000

    Allow devdeputies.com hosts in Rails preview

--- example-apps/rails-with-auth/config/environments/preview.rb
+++ example-apps/rails-with-auth/config/environments/preview.rb
@@ -4,6 +4,7 @@ Rails.application.configure do
   # Allow preview and proxy hosts used by Deputies and Daytona sandboxes.
   config.hosts << /\A([a-zA-Z0-9-]+\.)*deputies\.localhost\z/
   config.hosts << /\A([a-zA-Z0-9-]+\.)*deputies\.dev\z/
+  config.hosts << /\A([a-zA-Z0-9-]+\.)*devdeputies\.com\z/
   config.hosts << /\Alocalhost(?::\d+)?\z/
   config.hosts << "127.0.0.1"
   config.hosts << /\A[a-zA-Z0-9-]+\.daytonaproxy01\.net\z/
